aa9d81e72b20c2d81dc320704af8b6a5431f43b5
yudisrywn <yudisuryawan7@students.unnes.ac.id>
2024-12-03 15:58:45 +0700

    mmebuat class minesweeper

---
+++ Minesweeper.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+//Program utama menjalankan game minesweeper
+public class Minesweeper {
+    
+    public static void main(String[] args) {
+        new Minesweeper(); //menjalankan program minesweeper
+    }
+}
